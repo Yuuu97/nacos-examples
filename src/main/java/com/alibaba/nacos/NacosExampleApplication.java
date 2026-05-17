d62ffe2e2b4a9bb7f26352e4b8479c5206c2865c
qinyu <qin.yu@cestc.cn>
2026-05-17 23:35:48 +0800

    first commit

---
+++ src/main/java/com/alibaba/nacos/NacosExampleApplication.java
@@ -0,0 +1,13 @@
+package com.alibaba.nacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosExampleApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(NacosExampleApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/com/alibaba/nacos/nacosexample/NacosExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alibaba.nacos.nacosexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NacosExampleApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
